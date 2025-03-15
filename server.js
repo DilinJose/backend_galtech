a69db6e8ddcba21c10c58219c056d764d5f65b70
DilinJose <102725748+DilinJose@users.noreply.github.com>
2025-03-15 07:07:54 +0000

    changed Folder structure

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+// const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const router = jsonServer.router(path.join(__dirname, "db.json")); // ✅ Now it's in root
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
@@ -10,7 +11,7 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
